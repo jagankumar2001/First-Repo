d8ac9b5b03f0fb7d6607efa372e46e8dc5a1e48f
jagankumar2001 <154983877+jagankumar2001@users.noreply.github.com>
2024-02-01 19:19:58 +0530

    Add files via upload

---
+++ final2.py
@@ -0,0 +1,195 @@
+import face_recognition
+import cv2
+import numpy as np
+import csv
+import os
+from datetime import datetime
+import pandas as pd
+import webbrowser
+from email.message import EmailMessage
+import smtplib
+from tkinter import *
+
+root = Tk()
+root.configure(background="White")
+root.geometry("1300x1300")
+root.title("Attendance system")
+
+datetime_obj = datetime.now()
+dte = datetime_obj.date()
+present_file_path = f"C:/Users/cjaga/students/{dte}_present.csv"
+absent_file_path = f"C:/Users/cjaga/students/{dte}_absent.csv"
+image_path = "C:/Users/cjaga/Desktop/face recognition system/Photos/"
+
+
+known_faces = ["jagan", "deekshith", "nithin"]
+for i in known_faces:
+    image = face_recognition.load_image_file(image_path + i + ".jpg")
+    exec(f"{i}_encoding= face_recognition.face_encodings(image)[0]")
+            
+known_face_encoding = [jagan_encoding,deekshith_encoding, nithin_encoding]
+
+known_faces_names = ["jagan", "deekshith", "nithin",]
+
+print("Starting video capture:")    
+def start_attendance():   
+	video_capture = cv2.VideoCapture(0)
+	students = known_faces_names.copy()
+	##To create csv files
+	# assign header columns
+	pre_headerList = ['Name', 'Time']
+	ab_headerList = ['Name']
+
+	# open CSV file and assign header
+	with open(present_file_path, 'w') as file1:
+		dw1 = csv.DictWriter(file1, delimiter=',', fieldnames=pre_headerList)
+		dw1.writeheader()
+	with open(absent_file_path, 'w') as file2:
+		dw2 = csv.DictWriter(file2, delimiter=',', fieldnames=ab_headerList)
+		dw2.writeheader()
+
+	file1.close()
+	file2.close()
+
+	face_locations = []
+	face_encodings = []
+	face_names = []
+	s = True
+
+	while True:
+		_, frame = video_capture.read()
+		small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)					
+		rgb_small_frame = small_frame[:, :, ::-1]
+		if s:
+			face_locations = face_recognition.face_locations(rgb_small_frame)
+			face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+			face_names = []
+			for face_encoding in face_encodings:
+				matches = face_recognition.compare_faces(known_face_encoding, face_encoding)
+				name = ""
+				face_distance = face_recognition.face_distance(known_face_encoding, face_encoding)
+				best_match_index = np.argmin(face_distance)
+				if matches[best_match_index]:
+					name = known_faces_names[best_match_index]
+
+				face_names.append(name)
+				if name in known_faces_names:
+					font = cv2.FONT_HERSHEY_SIMPLEX
+					bottomLeftCornerOfText = (10, 100)
+					fontScale = 1.5
+					fontColor = (255, 0, 0)
+					thickness = 3
+					lineType = 2
+
+					cv2.putText(frame, name + ' Present',
+					bottomLeftCornerOfText,
+								font,
+								fontScale,
+								fontColor,
+								thickness,
+								lineType)
+					if name in students:
+						students.remove(name)
+						#print(students)
+						present_students = []
+						for j in known_faces_names:
+							if j not in students:
+								present_students.append(j)
+
+						now = datetime.now()
+						current_time = now.strftime("%H:%M %p")
+
+						#present students
+						for x in present_students:
+							with open(present_file_path, 'a') as file3:
+								dw3 = csv.DictWriter(file3, delimiter=',', fieldnames=pre_headerList, lineterminator='\n')
+								dw3.writerow({'Name': x, 'Time': current_time})
+
+		cv2.imshow("attendence system", frame)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+    ##absent students
+	for y in students:
+		with open(absent_file_path, 'a') as file4:
+			dw3 = csv.DictWriter(file4, delimiter=',', fieldnames=ab_headerList, lineterminator='\n')
+			dw3.writerow({'Name': y})
+
+	##Dropping duplicate rows
+	df_state = pd.read_csv(present_file_path)
+	Dup_Rows = df_state[df_state.duplicated()]
+	DF_RM_DUP1 = df_state.drop_duplicates(keep='first')
+	DF_RM_DUP1.to_csv(present_file_path, mode="w", index=False)
+
+	video_capture.release()
+	cv2.destroyAllWindows()
+	return (students)
+
+##HTML PART
+def view_records():
+	f = open("Heading.html", "w")
+	file_pr = pd.read_csv(present_file_path)
+	file_pr.to_html(f"C:/Users/cjaga/students/{dte}_present.html")
+	file_ab = pd.read_csv(absent_file_path)
+	file_ab.to_html(f"C:/Users/cjaga/students/{dte}_absent.html")
+	message = """
+	<html>
+	<head>
+	</head>
+	<body>
+	<center>
+	<h1><b>Attendance System</h1>
+	<a href="C:/Users/cjaga/students">Click to see Student record</a>
+	</body>
+	</html>
+	"""
+	f.write(message)
+	f.close()
+	webbrowser.open_new_tab("Heading.html")
+	
+def email_alert(subject, body, to):
+	msg = EmailMessage()
+	msg.set_content(body)
+	msg["subject"] = subject
+	msg["to"] = to
+	
+	user = "cjagankumar2001@gmail.com"
+	msg["from"] = user
+	password = "vexqvdonfaqfzrbi"
+	
+	server = smtplib.SMTP("smtp.gmail.com", 587)
+	server.starttls()
+	server.login(user, password)
+	server.send_message(msg)
+	server.quit()
+             
+### MAIN##########################
+label1= Label(root,text="Face recognition attendance system",fg="black",bg="white",width=40,font=("Times",30,"bold"))
+label1.grid(row=0,column=0,padx=30,pady=30)
+
+# Create a button
+button1 = Button(root, text="start_attendance", command=start_attendance(),fg="blue",bg="white")
+button1.grid(row=3,column=1,padx=30,pady=10)
+
+button2 = Button(root, text="View records", command=view_records(),fg="blue",bg="white")
+button2.grid(row=4,column=1,padx=30,pady=10)
+  
+root.mainloop()
+
+def emial_al():
+	absent_students=start_attendance()
+	view_records()
+	email_list=[]
+	for x in absent_students:
+		with open('email.csv', 'r') as file:
+			reader = csv.DictReader(file, fieldnames=['Name', 'Email', 'Register no'])
+			next(reader, None)
+			for row in reader:
+				if row['Name'] == x:
+					print(f"Sending email to {row['Name']}")
+					email_alert(f"Absent on {dte}", f"Dear {row['Name']} ({row['Register no']})\n,We missed you again today!\nWe are looking forward to seeing you in next class\n\n Thankyou,\nJagan", row['Email'])
+				
+				
+
+
+
